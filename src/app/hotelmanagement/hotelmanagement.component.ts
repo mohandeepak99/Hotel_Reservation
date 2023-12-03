26e8fc122decb58c1a36ad5d0cf8c355eb99909e
mohandeepak99 <75600610+mohandeepak99@users.noreply.github.com>
2023-12-02 23:17:45 -0500

    hotelmanagement.component.ts

--- src/app/hotelmanagement/hotelmanagement.component.ts
+++ src/app/hotelmanagement/hotelmanagement.component.ts
@@ -31,27 +31,7 @@ export class HotelmanagementComponent implements OnInit {
 
   }
 
-  deletehotel(hotelid:number){
-
-    if(confirm("Are you sure want to delete this hotel"))
-    {
-     this.userSer.deletesinglehoteldata(hotelid).subscribe((data:string)=>{
-
-      console.log(data);
-
-      this.msg=data;
-      var index=this.hotels.findIndex((obj)=>{
-
-        return obj._id==hotelid;
-      });
  
-      this.hotels.splice(index,1);      
-
-     },(error:any)=>{
-      this.msg="something went wrong";
-     })
-    }
-  }
   dosearch(search:string){
 
     this.userSer.searchhotel(search).subscribe((data:any[])=>{
